74f1bce383d8bbda660d5a494b3f88a4912ff495
Edwin <softwareywebsoluciones@gmail.com>
2015-01-26 00:42:00 -0500

    Agregado Ejemplo XML
    Agregado Ejemplo XML

---
+++ Vista/Facturacion/index.php
@@ -0,0 +1,12 @@
+<?php include ('../header.php') ?>
+	<div class="row">
+		<div class="col-md-12">
+	    	<div class="page-header" style="text-align:center;">
+			  <h1>Facturación</small></h1>
+			</div>
+	    </div>
+	</div>
+	<div class="row">
+		<a href="<?php echo $domain; ?>/Vista/Facturacion/XML">EJEMPLO GENERACION XML</a>
+	</div>
+<?php include ('../footer.php') ?>
